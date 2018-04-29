9f7d012290fcb040fdd3819616506e15facc8e2a
Guillermo Angeris <guillean@stanford.edu>
2018-04-29 15:03:09 -0700

    some more probably useful documentation

--- localization/localization_util.py
+++ localization/localization_util.py
@@ -5,9 +5,14 @@ import numpy as np
 RADIUS_OF_EARTH = 6371e3
 DEGREES = 360
 
+
 def to_global_system(plane_latlong, obj_xy, plane_orientation):
     '''Convert plane latlong, plane_orientation, and object_xy to latlong.
-    
+
+    This function assumes object_xy is a np.array of length 2 containing the 
+    distance (x,y) pair, in meters, of the object relative to the plane
+    (in the frame of the plane's point of view).
+
     plane_orientation is relative to north
 
     NOTE: This function assumes the curvature is negligible!
@@ -16,25 +21,27 @@ def to_global_system(plane_latlong, obj_xy, plane_orientation):
     world_rm = rot_mat(-to_rad(plane_orientation))
 
     return plane_latlong + world_rm.dot(obj_latlong)
-    
 
 
 def to_xy(latlong):
     ''' Converts latlong to xy.
-    
+
     NOTE: This function assumes the curvature is negligible!
     '''
     return 2*np.pi*RADIUS_OF_EARTH*origin_latlong/DEGREES
 
+
 def to_latlong(xy):
     ''' Converts latlong to xy.
-    
+
     NOTE: This function assumes the curvature is negligible!
     '''
     return DEGREES*xy/(2*np.pi*RADIUS_OF_EARTH)
 
+
 def to_rad(theta):
     return 2*np.pi*theta/DEGREES
+
 
 def rot_mat(theta):
     return np.array([
