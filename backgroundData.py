ba08d8a0077b2e3da5a6d97abb0a47d44fb5af2e
Josh-Payne <snare117@gmail.com>
2018-05-03 15:40:28 -0700

    pseudocode

--- backgroundData.py
+++ backgroundData.py
@@ -13,3 +13,15 @@ for r in range(4):
             h,w,channels=crop_img.shape
             if h==50 and w==50:
                 cv2.imwrite("img1/background" + "r" + str(r)+"x"+str(x)+"y"+str(y) + ".png", crop_img)
+
+# Next step: 
+#   for image in ./img1:
+#       s = np.random.randn(10)
+#       shape = cv.imread ("shape"+str(s))
+#       newShape = keras.rotate(shape, random degrees)
+#       r = np.random.randn(500000) # about 700000 images in the EMNIST, less if we only count capital letters and numbers
+#       name = EMNIST[r].name
+#       letter = EMNIST[r].image
+#       shape = cv2.copyTo(keras.rotate(letter, random degrees), shape, NO_TRANSLATION,NO_ROTATION)
+#       newImage = cv2.copyTo(shape,image,SOME_TRANSLATION,SOME_ROTATION)
+#       cv2.imwrite("../newimgs/"+"letter"+letter+"shape"+str(s)+"index"+str(index)+".png",newImage)
