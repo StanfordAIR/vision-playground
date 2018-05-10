fd7dce6ac054a397b98e48829b8b3340f1fc729e
Josh-Payne <snare117@gmail.com>
2018-05-10 00:53:42 -0700

    new ds generator

--- emnist-dataset-generator/bg.py
+++ emnist-dataset-generator/bg.py
@@ -1,7 +1,8 @@
 import cv2
 from scipy import ndimage
 
-img = cv2.imread("./test.png")
+img = cv2.imread("field-images/field.jpg")
+i = 0
 for r in range(4):
     rotated = ndimage.rotate(img, r*90)
     height, width, channels = img.shape 
@@ -9,10 +10,11 @@ for r in range(4):
     yScale = int((height)/10)
     for x in range(xScale):
         for y in range(yScale):
-            crop_img = img[10*y:10*y+50, 10*x:10*x+50]
+            crop_img = img[10*y:10*y+100, 10*x:10*x+100]
             h,w,channels=crop_img.shape
-            if h==50 and w==50:
-                cv2.imwrite("backgrounds/background" + "r" + str(r)+"x"+str(x)+"y"+str(y) + ".png", crop_img)
+            if h==100 and w==100:
+                cv2.imwrite("backgrounds/background"+str(i)+".png", crop_img)
+                i += 1
 
 # Next step: 
 #   for image in ./backgrounds:
@@ -25,3 +27,4 @@ for r in range(4):
 #       shape = cv2.copyTo(keras.rotate(letter, random degrees), shape, NO_TRANSLATION,NO_ROTATION)
 #       newImage = cv2.copyTo(shape,image,SOME_TRANSLATION,SOME_ROTATION)
 #       cv2.imwrite("../newimgs/"+"letter"+letter+"shape"+str(s)+"index"+str(index)+".png",newImage)
+
