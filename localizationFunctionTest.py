f4c8e8b8cd7320be7af9178f9b4b4ea7fb68798b
Peggy (Yuchun) Wang <peggy.yuchun.wang@gmail.com>
2018-05-18 15:13:19 -0700

    update

--- localizationFunctionTest.py
+++ localizationFunctionTest.py
@@ -2,9 +2,8 @@ import cv2
 import numpy as np
 import sys
 from matplotlib import pyplot as plt
+from RectArrayFunctionPeggy import rectArrayReturn
 
-# img = cv2.imread('robot.jpg',0)
-# img = cv2.imread('butterfly.png', 0)
 img = cv2.imread('temp.png', 0)
 
 cv2.imshow('whatever', img)
@@ -22,6 +21,16 @@ keypointsList = surf.detect(img, None)
 # surf.setUpright(True)
 
 img2 = cv2.drawKeypoints(img, keypointsList, None, (255, 0, 0), 4)
+
+potentialPointsList = rectArrayReturn(keypointsList)
+# print(potentialPointsList[0].x, potentialPointsList[0].y, potentialPointsList[0].length, potentialPointsList[0].width)
+
+for rect in potentialPointsList:
+    print(rect.x, rect.y, rect.length, rect.width)
+    img3 = cv2.rectangle(img, (int(rect.x), int(rect.y)), (int(rect.x + rect.length), int(rect.y + rect.width)), (255, 0, 0))
+    cv2.imshow('test', img3)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 # cv2.destroyAllWindows()
 plt.imshow(img2), plt.show()
