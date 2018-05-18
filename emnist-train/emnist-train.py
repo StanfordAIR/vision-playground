d23161f4eba4e8bbf8b3aec80a48a44bfeb9eb14
Josh-Payne <snare117@gmail.com>
2018-05-18 16:12:08 -0700

    prediction retriever on random letter

--- emnist-train/emnist-train.py
+++ emnist-train/emnist-train.py
@@ -8,18 +8,17 @@ import keras as K
 train_db = pd.read_csv("data/emnist-balanced-train.csv")
 test_db  = pd.read_csv("data/emnist-balanced-test.csv")
 
+charInd = random.randint(0,112000)
+emnist = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabdefghnqrt"
+
 num_classes = 47
 y_train = train_db.iloc[:,0]
 y_train = K.utils.np_utils.to_categorical(y_train, num_classes)
-print ("y_train:", y_train.shape)
 
 x_train = train_db.iloc[:,1:]
 x_train = x_train.astype('float32')
 x_train /= 255
-print ("x_train:",x_train.shape)
 
-emnist = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabdefghnqrt"
-charInd = random.randint(0,112000)
 alphanum = np.where(y_train[charInd]==1.)[0][0]
 print(emnist[alphanum])
 
@@ -36,7 +35,7 @@ model.compile(loss='categorical_crossentropy', # using the cross-entropy loss fu
               metrics=['accuracy']) # reporting the accuracy
 
 model.fit(x_train, y_train, # Train the model using the training set...
-          batch_size=512, epochs=10,
+          batch_size=512, epochs=1,
           verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
 
 y_test = test_db.iloc[:,0]
@@ -50,8 +49,7 @@ print ("x_test:",x_train.shape)
 
 print(model.evaluate(x_test, y_test, verbose=1)) # Evaluate the trained model on the test set
 
-let = x_train[charInd]
-let = let.reshape(1,1,28,28)
+let = np.array([x_train.iloc[charInd,:]])
+
 prediction = model.predict(let)
 print(prediction)
-print(emnist[prediction])
